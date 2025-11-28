94af70dd8f5a51ae3cc3520ea34ba247fa01aba6
James Woglom <j@wogloms.net>
2025-11-28 07:39:46 +0200

    Fix KeyboardOptions import for sound settings

--- mobile/src/main/java/com/jwoglom/controlx2/presentation/screens/sections/SoundSettingsActions.kt
+++ mobile/src/main/java/com/jwoglom/controlx2/presentation/screens/sections/SoundSettingsActions.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
@@ -40,7 +41,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
-import androidx.compose.ui.text.input.KeyboardOptions
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
